6cf83a80acdb305231497805dc77c239b8e11165
Sumukha Ramesh <96346340+sumukharamesh@users.noreply.github.com>
2023-05-12 15:51:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -23,17 +23,20 @@ def dbcon():
     return conn
 
 def user(conn):
+    res = []
     cursor = conn.cursor()
     query = "SELECT username FROM user"
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
         print(result[0])
+        res.append(result[0])
     user1 = input("Enter the username: ")
+    while user1 not in res:
+        user1 = input("Username not found. Re-enter username: ")
     sql = "SELECT host, port, username, password, path FROM user WHERE username = ?"
     cursor.execute(sql, (user1,))
     result = cursor.fetchone()
-    print(len(result))
     return result
 
 
@@ -65,7 +68,7 @@ def connectionbuild(s_host,s_port,s_username,s_password):
 
     ssh_client.connect(hostname=s_host, port=s_port, username=s_username, password=s_password)
 
-
+    return ssh_client
 
 
 
@@ -87,15 +90,15 @@ def select():
         files = os.listdir(folder_path)
 
     name = input("\n \n Enter the name of the file from the above list: ")
-    if name in files:
-        return name
-    else:
-        print("File not found")
+    while True:
+        if name in files:
+            return name
+        name = input("\n \n File not found. Please re-enter: ")
 
 
 def installer(i,s_host,s_port,s_username,s_password):
     ssh_client = connectionbuild(s_host,s_port,s_username,s_password)
-    cmd = f'echo "2416109" | sudo -S apt install ./{i} -y'
+    cmd = f'echo {s_password} | sudo -S apt install ./{i} -y'
     stdin, stdout, stderr = ssh_client.exec_command(cmd)
     err = stderr.read().decode("utf-8")
     output = stdout.read().decode("utf-8")
@@ -104,7 +107,7 @@ def installer(i,s_host,s_port,s_username,s_password):
 
 def remove(i,s_host,s_port,s_username,s_password):
     ssh_client = connectionbuild(s_host,s_port,s_username,s_password)
-    cmd = f'echo "2416109" | sudo -S apt remove {i} -y'
+    cmd = f'echo {s_password} | sudo -S apt remove {i} -y'
     stdin, stdout, stderr = ssh_client.exec_command(cmd)
     output = stdout.read().decode("utf-8")
     print(output)
@@ -142,48 +145,59 @@ def receive(s_host,s_port,s_username,s_password):
     # Create an SCP client
     ssh_client = connectionbuild(s_host,s_port,s_username,s_password)
     scp = SCPClient(ssh_client.get_transport())
-    remote_path =     input = ("Enter the path of the file: ")
+    remote_path = input("Enter the path of the file: ")
     local_path = r'C:\Users\USER\Desktop'
     scp.get(remote_path, local_path)
     scp.close()
     ssh_client.close()
 
 
-def main():
-
-   while(True):
-    os.system('cls')
-    print("\t\t\t______________________MULTI-SYSTEM_INSTALLER______________________")
-    cmd = input("\nEnter '1' to list the systems  \n\nEnter '2' to select operation \n\nEnter '3' to Exit ")
-
-    if cmd == '1':
-     os.system('cls')
-     conn = dbcon()
-     s_host,s_port,s_username,s_password,s_path =  user(conn)
-     
-    elif cmd == '2':
+def call():
+    while(True):
         os.system('cls')
-        cmd1 = input("\n  '4' : file send \n  '5' : file receive \n  '6' : install software \n  '7' : remove software' \n  '8' : List users \n")
-        if cmd1 == '4':
-            name = select()
-            send(name,s_host,s_port,s_username,s_password,s_path)
-        elif cmd1 == '5':
-            receive(s_host,s_port,s_username,s_password)
-        elif cmd1 == '6':
-            name = select()
-            installer(name,s_host,s_port,s_username,s_password) 
-        elif cmd1 == '7':
-            name = input("Enter the file name : ")
-            remove(name,s_host,s_port,s_username,s_password)   
-        elif cmd1== '8':
-            accessdata()        
+        print("\t\t\t______________________MULTI-SYSTEM_INSTALLER______________________")
+        cmd = input("\nEnter '1' to change the user  \n\nEnter '2' to select operation \n\nEnter '3' to Exit ")
 
-    elif(cmd == '3'):
-        conn.close()
-        break
-    else:
-        print("\n Invalid keyword")
-        main()
+        if cmd == '1':
+            os.system('cls')
+            conn = dbcon()
+            s_host,s_port,s_username,s_password,s_path =  user(conn)
+            conn.close()
+            
+        elif cmd == '2':
+            os.system('cls')
+            cmd1 = input("\n  '4' : file send \n  '5' : file receive \n  '6' : install software \n  '7' : remove software' \n  '8' : List users \n")
+            if cmd1 == '4':
+                name = select()
+                send(name,s_host,s_port,s_username,s_password,s_path)
+            elif cmd1 == '5':
+                receive(s_host,s_port,s_username,s_password)
+            elif cmd1 == '6':
+                name = select()
+                installer(name,s_host,s_port,s_username,s_password) 
+            elif cmd1 == '7':
+                name = input("Enter the file name : ")
+                remove(name,s_host,s_port,s_username,s_password)   
+            elif cmd1== '8':
+                accessdata()        
+
+        elif(cmd == '3'):
+            break
+        else:
+            print("\n Invalid keyword")
+            main()
+
+def main():
+   
+    os.system('cls')
+    print("\t\t\t_____________________MULTI-SYSTEM_INSTALLER______________________")
+    conn = dbcon()
+    s_host,s_port,s_username,s_password,s_path =  user(conn)
+    conn.close()
+    
+    call()
+   
+    
 
 if __name__ == "__main__":    
     main()
--- user.py
+++ user.py
@@ -17,11 +17,16 @@ sql='''CREATE TABLE user (
 cursor.execute(sql)
 
 cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (1,'169.254.170.114',22,'ubuntu','ubuntu@1','/home/ubuntu'))
-cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (2,'169.322.121.212',22,'reshma01','reshma','Reshmakka'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (2,'192.168.251.218',22,'reshmar','pass1234word','/home/reshmar'))
 cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (3,'192.168.0.106',22,'sumukha0','2416109','/home/sumukha0'))
 cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (4,'192.168.0.107',22,'sumukha1','2416109','/home/sumukha1'))
 cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (5,'169.254.170.114',22,'chirag','chirag01','chirag1'))
 cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (6,'192.168.02.106',22,'amogh','passwordword','amogh'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (7,'10.60.210.124',22,'ml20\student','student','amogh'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (8,'10.60.210.130',22,'ml23\student','student','amogh'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (9,'10.60.223.113',22,'ml1\student','student','amogh'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (10,'10.60.210.151',22,'ml13\student','student','amogh'))
+cursor.execute("insert into user (id,host,port,username,password,path)  values(?, ?, ?, ?, ?, ?)", (11,'10.60.210.127',22,'ml21\student','student','amogh'))
 # commit the changes to the database
 conn.commit()
 
